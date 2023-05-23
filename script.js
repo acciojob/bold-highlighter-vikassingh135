2e308cf3a9a3f5162a3463762696abd95d99fd6a
Vikas Singh <69408149+vikassingh135@users.noreply.github.com>
2023-05-24 00:15:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+
+let bolds = document.getElementsByTagName("strong");
+
 function highlight() {
     //Write your code here
-
+  for(let i=0; i<bolds.length; i++) {
+	  bolds[i].style.color = "green";
+  }
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+  for(let i=0; i<bolds.length; i++) {
+	  bolds[i].style.color = "black";
+  }
     
 }
